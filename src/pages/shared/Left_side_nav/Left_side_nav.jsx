585a3d7045c02eae255f36299432c95be4216344
Mohammad Ashif <mohammadashif510@gmail.com>
2023-10-05 18:52:32 +0600

    category data loaded

--- src/pages/shared/Left_side_nav/Left_side_nav.jsx
+++ src/pages/shared/Left_side_nav/Left_side_nav.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Left_side_nav = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        const url = "categories.json";
+        const loadCategoryData = async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            setCategories(data);
+        };
+
+        loadCategoryData();
+    }, []);
     return (
         <div>
-            <h1>Left side nav</h1>
+            <h1>All Category</h1>
+            <div className="mt-8">
+                {categories.map((category) => (
+                    <Link className="block py-3" key={category.id}>
+                        {category.name}
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
